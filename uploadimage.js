af082ff787eaafe58b58ab1b206d31b79be7bf7d
DESKTOP-QHP1O2H\MIKE <michael.okechukwu@chiefsoft.com>
2023-02-14 22:46:21 +0100

    image set up

---
+++ uploadimage.js
@@ -0,0 +1,14 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    CLOUDINARY_NAME : dxowmoejq,
+CLOUDINARY_API_KEY : 918892587334833,
+CLOUDINARY_API_SECRET : Q6z6nOJ9tABHi1CedmW7ctiiBYc,
+});
+
+const opts = {
+    overwrite: true,
+    invalidate: true,
+    resource_type: "auto",
+};
+
